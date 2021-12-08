76b6405b2e9943a7db95bbec5265841af74a5697
Ousseynou Mbaye <ouzinem@hotmail.com>
2021-12-07 20:26:52 -0500

    commit all project

---
+++ vue-by-html-file/app.js
@@ -0,0 +1,11 @@
+Vue.createApp({
+    data() {
+        return {
+            title: "Learn Vue33",
+            name: "",
+            montant :152,
+            isError : true
+
+        }
+    }
+}).mount('#app') 
